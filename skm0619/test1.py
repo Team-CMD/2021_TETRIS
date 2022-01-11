7ae4aa3f927131ac9f0ed752758ec24682d9aecc
skm0619 <74863778+skm0619@users.noreply.github.com>
2022-01-11 20:10:58 +0900

    second commit

--- skm0619/test1.py
+++ skm0619/test1.py
@@ -1,21 +1,21 @@
-import sys
-import pygame
-from pygame.locals import *
+import sys, pygame
 from math import sqrt
 from random import randint
 
-clock = pygame.time.Clock()
+pygame.init()
 screen = pygame.display.set_mode([600, 600])
+pygame.display.set_caption("Tetris")
 
-Width = 14
-Height = 21
-Field = []
-Psize = 24
-Pgsize = Psize + 1
-
+clock = pygame.time.Clock()
+WIDTH = 12
+HEIGHT = 22
+INTERVAL = 40
+FIELD = []
 COLORS = ((0, 0, 0), (255, 165, 0), (0, 0, 255), (0, 255, 255), (0, 255, 0), (255, 0, 255), (255, 255, 0), (255, 0, 0), (128, 128, 128))
 BLOCK = None
-NBlock = None
+NEXT_BLOCK = None
+PSIZE = 24 
+PGSIZE = PSIZE + 1 
 BLOCK_DATA = (
     (
         ( ## ㅣ모양
@@ -131,61 +131,78 @@ BLOCK_DATA = (
         )
     )
 )
+def Set_Field():
+    for i in range(HEIGHT - 1):
+        FIELD.insert(0, [-1, 0,0,0,0,0,0,0,0,0,0 ,-1])    
+    FIELD.insert(HEIGHT - 1, [-1, -1,-1,-1,-1,-1,-1,-1,-1,-1,-1 ,-1])
 
-class Setting(object):
-    def Init(self):   
-        pygame.init()
-        pygame.display.set_caption("Tetris")
-        done = False
-        
-    def Playing(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-    def Set_Field(self):
-        for i in range(Height - 1):
-            Field.insert(0, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
-        Field.insert(Height - 1, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-
-    def Draw_Field(self):
-        for i in range(Height):
-            for j in range(Width):
-                val = Field[i][j]
-                color = COLORS[val]
-                pygame.draw.rect(screen, color,(Pgsize + j * Pgsize, Pgsize + i * Pgsize, Psize, Psize))
-
-    def Start(self):
-        while True:
-            clock.tick(10)
-            self.Set_Field()
-            self.Draw_Field()
-            self.Playing()
-            pygame.display.flip()
+def Draw_Field():
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            val = FIELD[y][x]
+            color = COLORS[val]
+            pygame.draw.rect(screen, color, (PGSIZE + x * PGSIZE, PGSIZE + y * PGSIZE, PSIZE, PSIZE ))
 
 class Block:
-    def Block_init(self):
-        self.size = int(sqrt(len(self.data)))
-        self.turn = 0
-        self.type = BLOCK_DATA[0]
+    def __init__(self, count):
+        self.turn = randint(0, 3) 
+        self.type = BLOCK_DATA[randint(0, 6)]  
         self.data = self.type[self.turn]
-        self.x = randint(2, 8 - self.size)
-        self.y = 1 - self.size
+        self.size = int(sqrt(len(self.data)))
+        self.xpos = randint(2, 8 - self.size)
+        self.ypos = 1 - self.size
+        self.fire = count + INTERVAL
 
-    def Draw_Block(self):
+    def update(self, count):
+        erased = 0
+        if is_overlapped(self.xpos, self.ypos + 1, self.turn):
+            for y in range(BLOCK.size):
+                for x in range(BLOCK.size):
+                    index = y * self.size + x
+                    val = BLOCK.data[index]
+                    if 0 <= self.ypos + y < HEIGHT and 0 <= self.xpos + x < WIDTH and val != 0:
+                            FIELD[self.ypos + y][self.xpos + x] = val
+
+        if self.fire < count:
+            self.fire = count + INTERVAL
+            self.ypos += 1
+        return erased
+
+    def draw(self):
         for y in range(self.size):
             for x in range(self.size):
                 index = y * self.size + x
                 val = self.data[index]
-                if 0 <= y + self.y < Height and 0 <= x + self.x < Width and val != 0:
-                    f_x = Pgsize + (x + self.x) * Pgsize
-                    f_y = Pgsize + (y + self.y) * Pgsize
-                    pygame.draw.rect(screen, COLORS[val], (f_x, f_y, Psize, Psize))
+                if 0 <= y + self.ypos < HEIGHT and 0 <= x + self.xpos < WIDTH and val != 0:
+                    f_xpos = PGSIZE + (x + self.xpos) * PGSIZE
+                    f_ypos = PGSIZE + (y + self.ypos) * PGSIZE
+                    pygame.draw.rect(screen, COLORS[val], (f_xpos, f_ypos, PSIZE, PSIZE))  
 
+def Next_block(count):
+    global BLOCK, NEXT_BLOCK
+    BLOCK = NEXT_BLOCK if NEXT_BLOCK != None else Block(count)
+    NEXT_BLOCK = Block(count)
 
-if __name__ == "__main__":
-    game = Setting()
-    game.Start()
+def is_overlapped(xpos, ypos, turn):
+    return False
 
+def main():
+    global INTERVAL
+    count = 0
+    
+    Next_block(INTERVAL)
+    Set_Field()
 
+    while True:
+        clock.tick(50)
+        count += 5
+        if count % 1000 == 0:
+            INTERVAL = max(1, INTERVAL - 2)
+        erased = BLOCK.update(count)
 
+        Draw_Field()
+        BLOCK.draw()
+        pygame.display.update()
+    
+main()
+pygame.quit()
